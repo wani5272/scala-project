55e052c0937f3533a0b162eb72c809de6983a488
wani5272 <205021230+wani5272@users.noreply.github.com>
2025-10-07 09:48:58 +0200

    fix to int exceptions

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,27 +3,52 @@ import scala.io.StdIn
 case class User(id: Int, name: String, age: Int, deleted: Boolean)
 
 object scalaApp {
-  // TODO: make immutable
   private var database: List[User] = List()
 
   // the following function is NOT purely functional (side effects)
   def readInput(prompt: String): String = {
-    println(prompt)
-    StdIn.readLine()
+    var input: String = ""
+    while (input == "") {
+      println(prompt)
+      input = StdIn.readLine()
+    }
+    input
+
+  }
+  def readInputAsInt(prompt: String): Integer = {
+
+    var input: Int = -1
+    while (input <= -1) {
+      println(prompt)
+      try {
+        input = StdIn.readLine().toInt
+      } catch {
+        case _ => input = -1
+      }
+    }
+    input
   }
 
   // Input: User Name, Age. Output: adds entry to database (not yet immutable)
   def addUser(name: String, age: Int): List[User] = {
     // determines the next ID, if empty
     val nextId = if (database.isEmpty) 1 else database.map(_.id).max + 1
-    // TODO: this line is dangerous: replacing the database with its new state. make immutable
     database = database :+ User(nextId, name, age, false)
     database
   }
 
+  def noUsersFound(): Unit  = {
+    println("No users found.")
+  }
+  def userWithIDAction(id: Int, action: String): Unit = {
+    println(s"User with ID $id $action")
+  }
+  def noUsersFoundWithID(id: Int): Unit = {
+    println(s"No users found with ID $id.")
+  }
   def listUsers(): List[User] = {
     if (!database.exists(_.deleted == false)) {
-      println("No users found.")
+      noUsersFound()
       List()
     } else {
       database.filter(_.deleted == false).foreach(user => println(s"ID: ${user.id}, Name: ${user.name}, Age: ${user.age}, deleted: ${user.deleted}"))
@@ -34,9 +59,9 @@ object scalaApp {
     database.filter(_.deleted == false).find(_.id == id) match {
       case Some(_) =>
         database = database.map(user => if (user.id == id & !user.deleted) user.copy(name = name, age = age, deleted = false) else user)
-        println(s"User with ID $id updated")
+        userWithIDAction(id, "updated")
       case None =>
-        println(s"No user found with ID $id.")
+        noUsersFoundWithID(id)
     }
     database
   }
@@ -44,10 +69,9 @@ object scalaApp {
     val initialSize = database.size
     database = database.map(user => if (user.id == id & !user.deleted) user.copy(deleted = true) else user)
     if (database.size < initialSize)
-      // TODO: Repetition? output should be in seperate functions (or main)
-      println(s"User with ID $id deleted.")
+      userWithIDAction(id, "deleted")
     else {
-      println(s"No user found with ID $id.")
+      noUsersFoundWithID(id)
     }
     database
   }
@@ -64,20 +88,20 @@ object scalaApp {
       readInput("Choose an option (1-5):") match {
         case "1" =>
           val name = readInput("Enter user name:")
-          val age = readInput("Enter user age:").toInt
+          val age = readInputAsInt("Enter user age:")
           addUser(name, age)
 
         case "2" =>
           listUsers()
 
         case "3" =>
-          val id = readInput("Enter user ID to update:").toInt
+          val id = readInputAsInt("Enter user ID to update:")
           val name = readInput("Enter new user name:")
-          val age = readInput("Enter new user age:").toInt
+          val age = readInputAsInt("Enter new user age:")
           updateUser(id, name, age)
 
         case "4" =>
-          val id = readInput("Enter user ID to delete:").toInt
+          val id = readInputAsInt("Enter user ID to delete:")
           deleteUser(id)
         case "5" =>
           running = false
